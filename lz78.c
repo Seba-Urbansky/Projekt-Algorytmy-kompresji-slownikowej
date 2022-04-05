ca45228f7d45543ad668b39e5da9263fae73c25c
Seba-Urbansky <68894278+Seba-Urbansky@users.noreply.github.com>
2022-04-05 22:42:40 +0200

    Add files via upload

--- lz78.c
+++ lz78.c
@@ -1,291 +1,277 @@
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 #include "lz78.h"
 
-extern void *malloc(int);
-
-/* Rozmiar słownika */
-#define HTSIZE 16384
-
-/* Słownik */
-int *larray;
-int *carray;
-int *codearray;
-
-/* Inicjalizacja słownika */
-
-int inicjalizuj(void)
+void DodajPotomek(lista **Dziecko, indeks idx, char znak)
 {
-    if (!(larray = malloc(sizeof(int) * HTSIZE)))
-        return -1;
-    if (!(carray = malloc(sizeof(int) * HTSIZE)))
-        return -1;
-    if (!(codearray = malloc(sizeof(int) * HTSIZE)))
-        return -1;
-    return 0;
+    if (*Dziecko == NULL)
+    {
+        *Dziecko = (lista *)malloc(sizeof(lista));
+        if (*Dziecko == NULL)
+        {
+            exit(0);
+        }
+        else
+        {
+            (*Dziecko)->Node.idx = idx;
+            (*Dziecko)->Node.znak = znak;
+            (*Dziecko)->Node.dziecko = NULL;
+            (*Dziecko)->pNext = NULL;
+        }
+    }
+    else
+    {
+        lista *biezacy = *Dziecko;
+
+        while (biezacy->pNext != NULL)
+            biezacy = biezacy->pNext;
+
+        biezacy->pNext = (lista *)malloc(sizeof(lista));
+        if (biezacy->pNext == NULL)
+        {
+            exit(0);
+        }
+        else
+        {
+            biezacy->pNext->Node.idx = idx;
+            biezacy->pNext->Node.znak = znak;
+            biezacy->pNext->Node.dziecko = NULL;
+            biezacy->pNext->pNext = NULL;
+        }
+    }
 }
 
-/* Kompresuj blok 'src' lub wielkość 'len' do buforu 'dst'.  Zwroc
- * skompresowany rozmiar bloku. */
-
-int kompresuj(unsigned char *dst, unsigned char *src, int len)
+lista *ZnajdzPotomka(lista *Dziecko, char znak)
 {
-    int x;
-    int l, c;
-    int count = 0;
-    int ln = len;
-    unsigned char *s = src;
-    unsigned char *d = dst;
-    unsigned assm = 0; /* Zamontuj bity*/
-    int nbits = 0;     /* Nie. Bity w bitach - zamontowane*/
-    int code = 256;    /* Dotychczasowy kod */
-    int end = 512;     /* Inny bit */
-    int bits = 9;      /* Bity potrzebne do kodu*/
-    int tmp;
+    if (Dziecko != NULL)
+    {
+        while (Dziecko != NULL && Dziecko->Node.znak != znak)
+            Dziecko = Dziecko->pNext;
+        if (Dziecko != NULL && Dziecko->Node.znak == znak)
+            return Dziecko;
+    }
+    return NULL;
+}
 
-    /* Obecny i wcześniejszy obiekt */
+drzewo *Znajdz(drzewo *Rodzic, char *znak, long long int *pozycja, long long int RozmiarNapisu)
+{
+    lista *Dziecko;
+    char character = znak[(*pozycja)];
 
-    if (!len) /* Pusty blok */
-        return 0;
+    // dop�ki nie mamy ko�ca listy dzieci
+    while (Rodzic != NULL)
+    {
+        Dziecko = ZnajdzPotomka(Rodzic->dziecko, character); // Zwraca element zawieraj�cy szukany znak (je�li nie ma takiego, zwraca null).
 
-    /* Sprawdzenie dla dlugosci kompresji */
-    c = s[0];
-    for (x = 0; x != len; ++x)
-        if (s[x] != c)
+        if ((*pozycja) < RozmiarNapisu - 1 && Dziecko != NULL)
+        {
+            (*pozycja)++;
+            Rodzic = &(Dziecko->Node);
+            character = znak[(*pozycja)];
+        }
+        else
             break;
-    if (x == len)
-    { /* Uruchom kodowanie dlugosci dla blokow */
-        *d++ = 0;
-        *d++ = c;
-        *d++ = len;
-        *d++ = (len >> 8);
-        return d - dst;
     }
-
-    /* Zainicjalizuj slownik*/
-    memset(carray, 255, sizeof(int) * HTSIZE);
-    memset(larray, 255, sizeof(int) * HTSIZE);
-    memset(codearray, 255, sizeof(int) * HTSIZE);
-
-    /* Kompresuj */
-
-    /* Otrzymaj pierwsze slowo */
-    l = *s++;
-    --ln;
-
-    /* Wyjsciowy typ kompresji */
-    *d++ = 2;
-
-    while (ln)
-    {
-        int hv;
-        c = *s++;
-        --ln;
-
-        /* Znajdowanie L+C w tablicy mieszajacej*/
-        for (hv = ((l << 8) ^ (c) ^ (l >> 6)) & (HTSIZE - 1); larray[hv] != -1; hv = (hv + 1) & (HTSIZE - 1))
-            if (larray[hv] == l && carray[hv] == c)
-                break;
-
-        if (larray[hv] != -1)
-        { /* Znalezione wejscie */
-            l = codearray[hv];
-        }
-        else
-        {
-            /* dodaj wejscie - l+c nie jest tablica mieszajaca */
-            larray[hv] = l;
-            carray[hv] = c;
-            codearray[hv] = code++;
-            if (code == end)
-                count = 3;
-            if (count > 0)
-                if (!--count)
-                    (end <<= 1), ++bits;
-
-            /* Wypisanie 'l' */
-            tmp = bits;
-            while (tmp)
-            {
-                assm |= (l << nbits);
-                if (tmp + nbits <= 16)
-                    nbits += tmp, tmp = 0;
-                else
-                    tmp -= (16 - nbits), l >>= (16 - nbits), nbits = 16;
-                while (nbits >= 8)
-                {
-                    *d++ = assm;
-                    assm >>= 8;
-                    nbits -= 8;
-                    if (d - dst == len)
-                        goto none;
-                }
-            }
-            /* Zacznij nowe typ string */
-            l = c;
-        }
-    }
-
-    /* Wyjscie ostatniego kodu */
-    tmp = bits;
-    while (tmp)
-    {
-        assm |= (l << nbits);
-        if (tmp + nbits <= 16)
-            nbits += tmp, tmp = 0;
-        else
-            tmp -= (16 - nbits), l >>= (16 - nbits), nbits = 16;
-        while (nbits >= 8)
-        {
-            *d++ = assm;
-            assm >>= 8;
-            nbits -= 8;
-            if (d - dst == len)
-                goto none;
-        }
-    }
-    while (nbits > 0)
-    {
-        *d++ = assm;
-        assm >>= 8;
-        nbits -= 8;
-        if (d - dst == len)
-            goto none;
-    }
-
-    return d - dst;
-
-
-none:
-    dst[0] = 1;                
-    memcpy(dst + 1, src, len); /* Skopiuj blok */
-    return len + 1;
+    return Rodzic;
 }
 
-/* Dekompresuj
- */
-
-int dekompresuj(unsigned char *dst, unsigned char *src, int len)
+void Kompresja(char *znak, long long int RozmiarNapisu, char *nazwa_pliku_wyjsciowego)
 {
-    if (!len) /* Bez danych */
-        return 0;
+    FILE *wyjsciowy = fopen(nazwa_pliku_wyjsciowego, "wb");
 
-    if (*src == 0) /* Zerowany blok*/
+    drzewo *Slownik = (drzewo *)malloc(sizeof(drzewo));
+    if (Slownik == NULL)
     {
-        int x;
-        int c = src[1];
-        len = src[2] + (src[3] << 8);
-        for (x = 0; x != len; ++x)
-            dst[x] = c;
-        return len;
+        exit(0);
     }
-
-    if (*src == 1) 
+    else
     {
-        if (len - 1)
-            memcpy(dst, src + 1, len - 1);
-        return len - 1;
-    }
+        Slownik->idx = 0;
+        Slownik->znak = NULL; // Jest niewa�ny
+        Slownik->dziecko = NULL;
 
-    if (*src == 2) /* lz78 */
-    {
-        int x, q, cnt;
+        long long int pozycja = 0;    // kt�ry z kolei znak rozpatrujemy.
+        long long int IndeksNowy = 1; // indeks zapisywany w slowniku.
 
-        /* Zamontowanie slowa */
-        unsigned long assm = 0;
-        int nbits = 0;
-
-        /* menedzer kodu */
-        int bits = 9;
-        int end = 512;
-
-        /* aktualny string */
-        int l, c;
-        int code;
-        unsigned char *d = dst;
-        code = 256;
-
-        /* dekomprsuj */
-
-        ++src;
-        --len;
-
-        /* Dostaj pierwsze slowo */
-        while (nbits < bits)
+        while (pozycja < RozmiarNapisu)
         {
-            assm |= ((unsigned long)*src++ << nbits);
-            --len;
-            nbits += 8;
-        }
-        l = (assm & (end - 1));
-        assm >>= bits;
-        nbits -= bits;
-        *d++ = l;
+            drzewo *Rodzic = Znajdz(Slownik, znak, &pozycja, RozmiarNapisu); // Szukamy, czy mamy ju� cz�� ci�gu.
 
-        while (len)
-        {
-            /* Dostan */
-            while (nbits < bits)
+            if (pozycja < RozmiarNapisu)
             {
-                assm |= ((unsigned long)*src++ << nbits);
-                --len;
-                nbits += 8;
-            }
-            c = (assm & (end - 1));
-            assm >>= bits;
-            nbits -= bits;
-
-            if (c < 256)
-            {
-                /* Dostan slowo*/
-                /* W tym punkcie trzeba wygenerowac nowy kod */
-                larray[code] = l;
-                carray[code] = c;
-                ++code;
-
-                /* Wypisanie */
-                *d++ = c;
-
-                /* Rozpocznij nowy string */
-                l = c;
+                if (IndeksNowy < UCHAR_MAX) // Sprawdzamy, czy osi�gni�to maksymalny rozmiar indeksu.
+                {
+                    fwrite(&(Rodzic->idx), sizeof(indeks), 1, wyjsciowy); // wpisuje indeksy do pliku
+                    fwrite(&znak[pozycja], sizeof(char), 1, wyjsciowy);   // wpisuje znaki do pliku
+                    DodajPotomek(&(Rodzic->dziecko), IndeksNowy, znak[pozycja]);
+                    pozycja++;
+                    IndeksNowy++;
+                }
+                else
+                {
+                    UsunSlownik(Slownik);
+                    exit(0);
+                }
             }
             else
-            {
-                /* Dostan kod*/
-
-                /* Musisz wygenerowac nowy kod w tym punkcie */
-                larray[code] = l;
-                for (q = larray[c]; q >= 256; q = larray[q])
-                    ;
-                carray[code] = q;
-                ++code;
-
-                /* Rozpocznij nowy string */
-                l = c;
-
-                /* Wypisanie */
-                cnt = 0;
-                while (larray[c] >= 256)
-                {
-                    ++cnt;
-                    *d++ = carray[c];
-                    c = larray[c];
-                }
-                *d++ = carray[c];
-                ++cnt;
-                *d++ = larray[c];
-                ++cnt;
-                for (x = 0; x != cnt / 2; ++x)
-                {
-                    q = d[-1 - x];
-                    d[-1 - x] = d[-cnt + x];
-                    d[-cnt + x] = q;
-                }
-            }
-            if (code == end)
-                end *= 2, bits += 1;
+                break;
         }
-        return d - dst;
+        UsunSlownik(Slownik);
+    }
+    fclose(wyjsciowy);
+}
+
+long long int DlugoscSlowa(indeks *idx, long long int pozycja)
+{
+    long long int licznik = 1;
+    indeks IndeksPoprzedni = idx[pozycja];
+
+    while (IndeksPoprzedni != 0)
+    {
+        IndeksPoprzedni = idx[IndeksPoprzedni - 1];
+        licznik++;
     }
 
-    return -1; /* Nie wiem jak to zdekompresowac */
+    return licznik;
+}
+
+char *CzytajSlowo(indeks *idx, char *znak, long long int pozycja, long long int RozmiarSlowa, char *slowo)
+{
+    indeks IndeksPoprzedni = idx[pozycja];
+    long long int pos = RozmiarSlowa - 1;
+    slowo[pos] = znak[pozycja];
+
+    while (IndeksPoprzedni != 0)
+    {
+        pos--;
+        slowo[pos] = znak[IndeksPoprzedni - 1];
+        IndeksPoprzedni = idx[IndeksPoprzedni - 1];
+    }
+
+    return slowo;
+}
+
+void Dekompresja(indeks *idx, char *znak, long long int RozmiarTablic, char *nazwa_pliku_wyjsciowego)
+{
+    FILE *wyjsciowy = fopen(nazwa_pliku_wyjsciowego, "wb");
+
+    long long int pozycja = 0;
+    while (pozycja < RozmiarTablic)
+    {
+        long long int RozmiarSlowa = DlugoscSlowa(idx, pozycja);
+        char *slowo = (char *)malloc(sizeof(char) * RozmiarSlowa);
+        slowo = CzytajSlowo(idx, znak, pozycja, RozmiarSlowa, slowo);
+        fwrite(slowo, sizeof(char), RozmiarSlowa, wyjsciowy);
+        free(slowo);
+        pozycja++;
+    }
+
+    fclose(wyjsciowy);
+}
+
+void WczytajKompresja(char *nazwa_pliku_wejsciowego, char *nazwa_pliku_wyjsciowego)
+{
+    // Otwieranie pliku.
+    FILE *plik = fopen(nazwa_pliku_wejsciowego, "rb");
+    if (plik == NULL)
+    {
+        exit(0);
+    }
+    else
+    {
+        fseek(plik, 0, SEEK_END);
+        long long int RozmiarPliku = ftell(plik);
+        fseek(plik, 0, 0);
+
+        // Alokacja pami�ci.
+        char *napis = (char *)malloc(sizeof(char) * RozmiarPliku);
+        if (napis == NULL)
+        {
+            exit(0);
+        }
+        else
+        {
+            // Kopiowanie zawarto�ci pliku do bufora.
+            long long int i = 0;
+            while (1)
+            {
+                fread(&napis[i], sizeof(char), 1, plik);
+                if (feof(plik))
+                    break;
+                i++;
+            }
+
+            Kompresja(napis, RozmiarPliku, nazwa_pliku_wyjsciowego);
+
+            // Uwolnienie bufora.
+            free(napis);
+
+            fclose(plik);
+        }
+    }
+}
+
+void WczytajDekompresja(char *nazwa_pliku_wejsciowego, char *nazwa_pliku_wyjsciowego)
+{
+
+    // Otwieranie pliku.
+    FILE *plik = fopen(nazwa_pliku_wejsciowego, "rb");
+    if (plik == NULL)
+    {
+        exit(0);
+    }
+    else
+    {
+        fseek(plik, 0, SEEK_END);
+        long long int RozmiarPliku = ftell(plik);
+        fseek(plik, 0, 0);
+        long long int RozmiarTablicy = RozmiarPliku / (sizeof(indeks) + sizeof(char));
+        // Alokacja pami�ci.
+        indeks *idx = (indeks *)malloc(sizeof(indeks) * RozmiarTablicy); // tablica indeks�w
+        char *znak = (char *)malloc(sizeof(char) * RozmiarTablicy);      // tablica znak�w
+        if (idx == NULL || znak == NULL)
+        {
+            exit(0);
+        }
+        else
+        {
+            // Kopiowanie zawarto�ci pliku do bufora.
+            long long int i = 0;
+            while (1)
+            {
+                fread(&idx[i], sizeof(indeks), 1, plik);
+                fread(&znak[i], sizeof(char), 1, plik);
+                if (feof(plik))
+                    break;
+                i++;
+            }
+
+            Dekompresja(idx, znak, RozmiarTablicy, nazwa_pliku_wyjsciowego);
+
+            // Uwolnienie bufora.
+            free(idx);
+            free(znak);
+
+            fclose(plik);
+        }
+    }
+}
+
+void UsunSlownik(drzewo *Drzewo)
+{
+    if (Drzewo != NULL)
+    {
+        lista *PoprzednieDziecko = NULL;
+        while (Drzewo->dziecko)
+        {
+            UsunSlownik(&(Drzewo->dziecko->Node));
+            PoprzednieDziecko = Drzewo->dziecko;
+            Drzewo->dziecko = Drzewo->dziecko->pNext;
+            free(PoprzednieDziecko);
+        }
+        free(Drzewo->dziecko);
+        UsunSlownik(Drzewo->dziecko);
+    }
 }
