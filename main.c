76f1e4ce89455c9c3fe96ac3ecc6da07a11de11d
Seba-Urbansky <68894278+Seba-Urbansky@users.noreply.github.com>
2022-04-05 22:43:11 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,44 +4,14 @@
 
 #include "lz78.h"
 
-#define MAX 2048
+#define MAX 32
 
 // to jest milestone1 - algorytm LZ78
 
 // http://fizyka.umk.pl/~leii/wskaznikiStrukturyAiR.pAf
 
-struct slownik
-{
-    int index;
-    struct slownik *znaki[100];
-}; // slownik lz78
-
-void menu_kompresuj(char* oryginalny_plik_nazwa, char* skompresowany_plik_nazwa);
-void menu_dekompresuj(char* oryginalny_plik_nazwa, char* skompresowany_plik_nazwa);
 void wyjscie();
 void menu();
-
-void menu_kompresuj(char* oryginalny_plik_nazwa, char* skompresowany_plik_nazwa)
-{
-    char tekst[MAX];
-    char resultat[MAX];
-    FILE *plik = fopen(oryginalny_plik_nazwa, "r");
-    fscanf(plik, "%s", &tekst);
-    kompresuj(&resultat, &tekst, strlen(tekst));
-    FILE *skompresowany_plik = fopen(skompresowany_plik_nazwa, "w");
-    fprintf(skompresowany_plik, "%s", resultat);
-}
-
-void menu_dekompresuj(char* oryginalny_plik_nazwa, char* skompresowany_plik_nazwa)
-{
-    char tekst[MAX];
-    char resultat[MAX];
-    FILE *plik = fopen(skompresowany_plik_nazwa, "r");
-    fscanf(plik, "%s", &tekst);
-    dekompresuj(&resultat, &tekst, strlen(tekst));
-    FILE *zdekompresowany_plik = fopen(oryginalny_plik_nazwa, "w");
-    fprintf(zdekompresowany_plik, "%s", resultat);
-}
 
 void wyjscie()
 {
@@ -49,7 +19,6 @@ void wyjscie()
 
 void menu()
 {
-    inicjalizuj();
     int wybor;
     char nazwa_pliku[MAX] = "LZ78.txt";
     char skompresowany_nazwa_pliku[MAX] = "LZ78.txt.compressed";
@@ -70,10 +39,10 @@ void menu()
     switch (wybor)
     {
     case 1:
-        menu_kompresuj(&nazwa_pliku, &skompresowany_nazwa_pliku);
+        WczytajKompresja(&nazwa_pliku, &skompresowany_nazwa_pliku);
         break;
     case 2:
-        menu_dekompresuj(&nazwa_pliku, &skompresowany_nazwa_pliku);
+        WczytajDekompresja(&skompresowany_nazwa_pliku, &nazwa_pliku);
         break;
     case 3:
         wyjscie();
